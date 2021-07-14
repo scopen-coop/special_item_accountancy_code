04693d71e43e532b77bb9d0e70d61fb1ffca2a09
Florian HENRY <florian.henry@open-concept.pro>
2021-07-14 19:19:18 +0200

    add all

---
+++ special_item_accountancy_code/special_item_accountancy_code/doctype/categorie_comptable_tiers/categorie_comptable_tiers.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, scopen.fr and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Categorie comptable Tiers', {
+	// refresh: function(frm) {
+
+	// }
+});
