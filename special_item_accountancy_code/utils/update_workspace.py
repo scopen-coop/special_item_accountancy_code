81e62c725fdc77185d63808e436e025dc60929ed
Florian HENRY <florian.henry@open-concept.pro>
2024-08-12 19:45:02 +0200

    migrate keep card in workspace

--- special_item_accountancy_code/utils/update_workspace.py
+++ special_item_accountancy_code/utils/update_workspace.py
@@ -16,7 +16,7 @@ def add_card(workspace_name, workspace_link_label):
     workspace = frappe.get_doc("Workspace", workspace_name)
 
     if not workspace:
-        return None
+        return
 
     workspace_link = frappe.get_last_doc(
         "Workspace Link",
@@ -31,7 +31,7 @@ def add_card(workspace_name, workspace_link_label):
             should_add_entry = False
 
     if not should_add_entry:
-        return None
+        return
 
     content.append(
         {
@@ -43,7 +43,6 @@ def add_card(workspace_name, workspace_link_label):
 
     workspace.content = json.dumps(content)
     workspace.save()
-    return 1
 
 
 def update_workspace_link_idx():
